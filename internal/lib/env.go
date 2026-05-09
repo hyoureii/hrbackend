a556cd897fa5956e190072d0ba02096d49cb55fa
Hyoureii <hyoureibynaufal@gmail.com>
2026-05-09 23:29:29 +0700

    updated .env.example and made helper for getting default env

--- internal/lib/env.go
+++ internal/lib/env.go
@@ -2,7 +2,14 @@ package lib
 
 import "os"
 
-func GetEnv(env, def string) string {
+func GetEnv(env string) string {
+	if foundEnv, found := os.LookupEnv(env); found {
+		return foundEnv
+	}
+	return ""
+}
+
+func GetEnvOrDefault(env, def string) string {
 	if foundEnv, found := os.LookupEnv(env); found {
 		return foundEnv
 	}
